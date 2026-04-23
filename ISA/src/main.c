d758838868fe50df945e865c771ab145b9ed39dd
Cuauhtemoc Herrera <cuauhtemoc.herrera@oracle.com>
2026-04-23 15:36:16 -0600

    Reorganize ISA project structure

--- ISA/src/main.c
+++ ISA/src/main.c
@@ -1,8 +1,44 @@
 #include <stdarg.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-const char *emulator_tag = "[v0.01][fly]cpuemu";
+const char *emulator_tag = "[cpuemu][v0.01|fly]";
+
+typedef struct {
+    unsigned short words[65536];
+} memory_t;
+
+typedef struct {
+    unsigned short registers[8];
+    memory_t memory;
+} cpu_t;
+
+void move(cpu_t *cpu, const char *dest, const char *src)
+{
+    int dest_register;
+    unsigned short value;
+
+    if (dest == NULL || src == NULL) {
+        return;
+    }
+
+    if (dest[0] != 'r' || dest[1] < '0' || dest[1] > '7' || dest[2] != '\0') {
+        return;
+    }
+
+    dest_register = dest[1] - '0';
+
+    if (src[0] == '#') {
+        value = (unsigned short) strtoul(src + 1, NULL, 0);
+    } else if (src[0] == 'r' && src[1] >= '0' && src[1] <= '7' && src[2] == '\0') {
+        value = cpu->registers[src[1] - '0'];
+    } else {
+        return;
+    }
+
+    cpu->registers[dest_register] = value;
+}
 
 void log_message(const char *message, ...)
 {
@@ -22,11 +58,18 @@ char *split_token(char *value)
     return strtok(value, " \r\n");
 }
 
+void left_pad_line_number(int line_number, char *value, size_t size)
+{
+    snprintf(value, size, "%5d", line_number);
+}
+
 int main(int argc, char **argv)
 {
     if (argc > 1) {
+        cpu_t cpu = {0};
         FILE *file;
         char line[1024];
+        char line_value[6];
         char split_line[1024];
         char *mnemonic;
         int line_number = 1;
@@ -45,19 +88,15 @@ int main(int argc, char **argv)
         log_message("");
 
         while (fgets(line, sizeof(line), file) != NULL) {
-            log_message("file::read::%d   %s", line_number, line);
+            left_pad_line_number(line_number, line_value, sizeof(line_value));
+            log_message("file::read::%s:: %s", line_value, line);
             strcpy(split_line, line);
             mnemonic = split_token(split_line);
-
-            if (mnemonic != NULL) {
-                log_message("%d mnemonic: %s", line_number, mnemonic);
-            }
-
             line_number++;
         }
 
         fclose(file);
-        log_message("file::read::EOF");
+        log_message("file::read::     :: EOF");
     }
 
     return 0;
